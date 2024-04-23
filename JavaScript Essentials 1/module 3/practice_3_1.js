1939d9f39bb392447e2b85d68252cf683849521b
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-04-23 02:27:00 -0400

    Add files via upload

---
+++ JavaScript Essentials 1/module 3/practice_3_1.js
@@ -0,0 +1,22 @@
+// Section practice
+
+console.log(2 * 3 + 1);      // expected 7
+console.log(2 ** 4);       // expected 16
+console.log(5 * 1);       // expected 5
+console.log(8 ** 2 - 5 ** 2); // expected 39
+
+// && operator will execute the first operand that is false, if not the second operand
+// || operator will execute the first operand that is true, if not the second operand
+
+// Fill in the missing operators in that such a way that all expressions result in true. (COMPARISON operators) 
+console.log(4 * 5 === 20);
+console.log(6 * 5 == "30");
+console.log(-17 < 0);
+console.log(25 > 1);
+console.log(2 + 2 * 2 != 4);
+
+// Fill in the missing operators in that such a way that all expressions result in true. (LOGICAL operators)
+console.log(true || false);
+console.log(false || ! false);
+console.log(false || false || true);
+console.log(true || false && false && true);
