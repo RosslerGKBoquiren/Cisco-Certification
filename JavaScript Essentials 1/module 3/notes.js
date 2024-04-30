02b8ebc4aadd664dda561a1ada11521ef62b8d11
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-04-30 00:11:57 -0400

    Add files via upload

--- JavaScript Essentials 1/module 3/notes.js
+++ JavaScript Essentials 1/module 3/notes.js
@@ -40,3 +40,33 @@ console.log("Alice" != "Bob"); // true
 console.log(0 !=  false); // false
 console.log(undefined != false); // true
 console.log(NaN != NaN); // true
+
+// >, <, >=, <=
+// The algorithm is complex and the comparison is not very useful when
+// comparing strings that do not represent numbers. 
+// To simplify, the higher the letter's position in the alphabet, the higher the
+// value. 
+// But, if its a capital letter, regardles of position, is less in value
+// than lower case.
+// Moreover, digits have even lower values than letters. 
+
+console.log("b" > "a"); // -> true
+console.log("a" > "B"); // -> true
+console.log("B" > "A"); // -> true
+console.log("A" > "4"); // -> true
+console.log("4" > "1"); // -> true
+ 
+console.log("ab1" < "ab4"); // -> true
+console.log("ab4" < "abA"); // -> true
+console.log("abB" < "aba"); // -> true
+console.log("aba" < "abb"); // -> true
+ 
+console.log("ab" < "ab4"); // -> true
+
+
+// Section practice
+let width = prompt("Volume of the box, enter width:");
+let height = prompt("Volume of the box, enter height");
+let length = prompt("Volume of the box, enter length");
+let volume = width * height * length;
+alert(`Calculated box volume is ${volume}`);
